a1c303adfd20e010bb5892221880d07f5d96df1b
i160229 Abid Waqar <i160229@nu.edu.pk>
2021-02-27 12:57:40 +0500

    solved 1 more problem

---
+++ Longest Increasing Subsequence Problem By Dynamic Programming/Longest Increasing Subsequence Problem.md
@@ -0,0 +1,11 @@
+# Longest Increasing Subsequence Problem By Dynamic Programming
+
+### Problem
+
+For example:
+
+In an array {10, 22, 9, 33, 21, 50, 41, 60}
+
+Longest increasing subsequence are: {10, 22, 33, 50, 60} or {10, 22, 33, 41, 60}
+
+The length of the LIS is 5
---
+++ Longest Increasing Subsequence Problem By Dynamic Programming/Solution/Solution/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Beast_Mode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length of array: ");
+            int arrayLength = int.Parse(Console.ReadLine());
+            int[] array = new int[arrayLength];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.Write("Input " + (i + 1) + ": ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("The length of LIS is " + findLongestIncreasingSubSequenceCount(array, arrayLength));
+        }
+
+        static int findLongestIncreasingSubSequenceCount(int[] array, int arrayLength)
+        {
+            int[] LIS = new int[arrayLength];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                LIS[i] = 1;
+            }
+
+            for (int i = 1; i < arrayLength; i++)
+            {
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        LIS[i] = Math.Max(LIS[i], LIS[j] + 1);
+                    }
+                }
+            }
+
+            int max = LIS[0];
+            for (int i = 1; i < arrayLength; i++)
+            {
+                max = Math.Max(max, LIS[i]);
+            }
+            return max;
+        }
+    }
+}
