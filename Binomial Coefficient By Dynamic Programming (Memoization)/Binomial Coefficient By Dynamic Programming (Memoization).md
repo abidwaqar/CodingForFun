73ef4dc3507e55e1ceea70d13fc46b03a4c0217e
i160229 Abid Waqar <i160229@nu.edu.pk>
2021-03-07 16:05:21 +0500

    added binomial coefficient by dynamic programming

---
+++ Binomial Coefficient By Dynamic Programming (Memoization)/Binomial Coefficient By Dynamic Programming (Memoization).md
@@ -0,0 +1,5 @@
+# Binomial Coefficient By Dynamic Programming (Memoization)
+
+### Problem
+
+*Write a function that takes two parameters n and k and returns the value of Binomial Coefficient C(n, k).* For example, your function should return 6 for n = 4 and k = 2, and it should return **10** for **n = 5** and **k = 2**.
---
+++ Binomial Coefficient By Dynamic Programming (Memoization)/Solution/Solution/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Beast_Mode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N:");
+            long N = long.Parse(Console.ReadLine());
+            Console.Write("K:");
+            long K = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("Binomial Coefficient is " + calculateBinomialCoefficient(N, K));
+        }
+
+        static Dictionary<string, long> memory = new Dictionary<string, long>();
+        static long calculateBinomialCoefficient(long N, long K)
+        {
+            if (K > N)
+            {
+                return 0;
+            }
+
+            if (K == 0 || K == N)
+            {
+                return 1;
+            }
+
+            if (!memory.ContainsKey(N + "C" + K))
+            {
+                memory[N + "C" + K] = calculateBinomialCoefficient(N - 1, K - 1) + calculateBinomialCoefficient(N - 1, K);
+            }
+
+            return memory[N + "C" + K];
+        }
+    }
+}
