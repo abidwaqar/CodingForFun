ae220455bd596ecec575a534217f4c4b869140ed
Abid Waqar <abidwaqar98@gmail.com>
2022-03-23 11:06:56 +0500

    kickstart round a 2022 3 questions

---
+++ SolutionTemplate.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SolutionTemplate {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int T = sc.nextInt();
+            for (int i = 1; i <= T; ++i) {
+                long A = sc.nextLong();
+                long B = sc.nextLong();
+
+                System.out.println("Case #" + i + ": " + solve(A, B));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static long solve(long A, long B) throws Exception {
+        return A + B;
+    }
+}
